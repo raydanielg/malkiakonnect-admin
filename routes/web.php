482cf551d5435f1bfac5401dcd3227fec462a863
raydanielg <airezra2+1@gmail.com>
2026-02-02 17:09:33 +0300

    commit

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+if (file_exists(base_path('Modules/Authmanagement/routes/web.php'))) {
+    require base_path('Modules/Authmanagement/routes/web.php');
+}
+
 Route::get('/', function () {
     return redirect()->route('auth.login');
 });
