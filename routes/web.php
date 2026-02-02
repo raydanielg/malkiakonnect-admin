9702355c138e072e267654e96459d8536be760a5
raydanielg <airezra2+1@gmail.com>
2026-02-02 20:52:39 +0300

    commit

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\View;
 
 if (file_exists(base_path('Modules/Authmanagement/routes/web.php'))) {
     require base_path('Modules/Authmanagement/routes/web.php');
+}
+
+if (is_dir(base_path('Modules/Adminmodules/resources/views'))) {
+    View::addNamespace('adminmodules', base_path('Modules/Adminmodules/resources/views'));
+}
+
+if (file_exists(base_path('Modules/Adminmodules/routes/web.php'))) {
+    require base_path('Modules/Adminmodules/routes/web.php');
 }
 
 Route::get('/', function () {
