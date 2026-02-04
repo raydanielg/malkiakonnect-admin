c8e936df219ac81adf6c9be497f68d6de7bee278
raydanielg <airezra2+1@gmail.com>
2026-02-05 00:23:47 +0300

    commit

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,16 @@ if (is_dir(base_path('Modules/Adminmodules/resources/views'))) {
     View::addNamespace('adminmodules', base_path('Modules/Adminmodules/resources/views'));
 }
 
+if (is_dir(base_path('Modules/Usermanagement/resources/views'))) {
+    View::addNamespace('usermanagement', base_path('Modules/Usermanagement/resources/views'));
+}
+
 if (file_exists(base_path('Modules/Adminmodules/routes/web.php'))) {
     require base_path('Modules/Adminmodules/routes/web.php');
+}
+
+if (file_exists(base_path('Modules/Usermanagement/routes/web.php'))) {
+    require base_path('Modules/Usermanagement/routes/web.php');
 }
 
 Route::get('/', function () {
